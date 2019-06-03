551b740d414a39adf4a1d7dcc22a08ebba3c8f2f
Patrick Longer <p.longer@gmx.de>
2019-06-03 16:21:23 +0200

    Adding 'createsettings' command and error handler

--- bot.py
+++ bot.py
@@ -7,8 +7,7 @@ import os
 
 token = os.environ['TOKEN']
 
-bot = commands.Bot("#")
-bot.remove_command("help")
+bot = commands.Bot("+")
 
 @bot.event
 async def on_ready():
@@ -18,10 +17,34 @@ async def on_ready():
         except IntegrityError as e:
             print("WARN: IntegrityError! Konnte Server {} | {} nicht erstellen!".format(guild.name, guild.id))
 
-@bot.event
-async def on_command_error(ctx):
-    pass
 
+
+@bot.command(pass_ctx=True)
+@commands.has_permissions(administrator=True)
+async def createsettings(ctx, logchid, botcchid, remoj, rcount, belvchid, banmsgchid, leavemsgchid, kickmsgchid, rcmsgchid, adminrole, roleonjoin, rssurl, rsschannelid):
+    valid_channels = []
+    ch_ids = [logchid, botcchid, belvchid, banmsgchid, leavemsgchid, kickmsgchid, rcmsgchid, rsschannelid]
+
+    for channel in ctx.guild.channels:
+        valid_channels.append(int(channel.id))
+
+    for channel in ch_ids:
+        if int(channel) not in valid_channels:
+            raise ValueError
+
+    sql.create_ssettings(ctx.guild.id, logchid, botcchid, remoj, rcount, belvchid, banmsgchid, leavemsgchid, kickmsgchid, rcmsgchid, adminrole, roleonjoin, rssurl, rsschannelid)
+
+@createsettings.error
+async def createsettings_error(ctx, error):
+    print(error)
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You dont have the permissons to create server settings!")
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("A required argument is missing!")
+    if "TypeError" in str(error):
+        await ctx.send("One of the arguments has got the wrong type (mixed up arguments?)")
+    if "ValueError" in str(error):
+        await ctx.send("One of the channel IDs are invalid or the bot cant see the channel")
 
 
 def get_guilds(): #get the guilds the bot is connected to
