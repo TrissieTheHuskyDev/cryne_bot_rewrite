31b8df561bcd57d54916212f76ef172100145435
Patrick Longer <p.longer@gmx.de>
2019-06-04 20:32:34 +0200

    Renaming to "helper.py", adding isInt check

--- helper.py
+++ helper.py
@@ -1,3 +1,12 @@
 def hasPerm(perm, int):
     return (perm & int) != 0
 
+def isInt(check, erroring = False):
+    try:
+        int(check)
+    except TypeError:
+        if erroring:
+            raise TypeError
+        else:
+            return False
+    return True
