ff26da1b6211906d0a28af24cd0408f00b4a275a
Patrick Longer <p.longer@gmx.de>
2019-06-09 22:09:56 +0200

    Adjusting message logging to take care of embeds | Adding member detection in on_ready() | Adding guild ban logic

--- cogs/events.py
+++ cogs/events.py
@@ -17,7 +17,10 @@ class Events(commands.Cog):
                 await message.channel.send(
                     "The prefix for this server currently is: " + str(sql.get_servers()[message.guild.id][1]))
 
-        sql.log_msg(message.id, message.guild.id, message.channel.id, now, message.content, message.author.id)
+        if message.content == "":
+            sql.log_msg(message.id, message.guild.id, message.channel.id, now, "<EMBED>", message.author.id, str(message.author))
+        else:
+            sql.log_msg(message.id, message.guild.id, message.channel.id, now, message.content, message.author.id, str(message.author))
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -27,8 +30,21 @@ class Events(commands.Cog):
             except IntegrityError as e:
                 print("WARN: IntegrityError! Konnte Server {} | {} nicht erstellen!".format(guild.name, guild.id))
 
+        for member in self.bot.users:
+            sql.delete_user(member.id)
+
+        for member in self.bot.users:
+            sql.register_user(member.id, member.name)
+
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
+        if sql.guild_is_banned(guild.id) == True:
+            print(sql.guild_is_banned(guild.id))
+            await guild.leave()
+            return
+
+        await guild.owner.send(f"It seems like you or something from your team has invited me to your Server `{guild.name}` Use `cb-createsettings` to configure me - then I'll be ready!")
+
         try:
             sql.create_server(guild.name, guild.id, "cb-")
         except IntegrityError as e:
