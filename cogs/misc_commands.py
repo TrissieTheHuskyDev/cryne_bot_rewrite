69b7ee8171fd0236eb36d3e773f414dba6894b4a
Patrick Longer <p.longer@gmx.de>
2019-06-09 22:10:55 +0200

    Adding command to get message log | Adding owner check

--- cogs/misc_commands.py
+++ cogs/misc_commands.py
@@ -1,8 +1,9 @@
 import discord
 from discord.ext import commands
-import asyncio
 
 import sql
+
+import os
 
 class Misc(commands.Cog):
     def __init__(self, bot):
@@ -20,6 +21,7 @@ class Misc(commands.Cog):
                         value="Currently present on " + str(len(self.bot.guilds)) + " guilds with " + str(len(self.bot.users)) + " members")
         await ctx.send(embed=embed)
 
+    @commands.is_owner()
     @commands.command(pass_ctx=True)
     async def register_users(self, ctx):
         for member in self.bot.users:
@@ -27,6 +29,23 @@ class Misc(commands.Cog):
 
         for member in self.bot.users:
             sql.register_user(member.id, member.name)
+
+    @commands.command(pass_ctx=True)
+    async def get_mlogs(self, ctx):
+        guild = ctx.guild.id
+
+        msglist = sql.get_gmsgs(guild)
+
+        with open(str(guild) + ".txt", "w") as logfile:
+            for entry in msglist:
+                logfile.writelines(entry)
+
+        with open(str(guild) + ".txt", "r") as logfile:
+            await ctx.author.send("Here is your message log:", file=discord.File(logfile, str(guild) + ".txt"))
+
+        os.remove(str(guild) + ".txt")
+
+
 
 def setup(bot):
     bot.add_cog(Misc(bot))
