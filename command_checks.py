1d22135a5c9c6f53deed0c0895c18c4e091ac3ae
Patrick Longer <p.longer@gmx.de>
2019-06-10 22:02:03 +0200

    Adding in_pm check

--- command_checks.py
+++ command_checks.py
@@ -13,3 +13,8 @@ def settings_created():
     async def predicate(ctx):
         return sql.settings_created(ctx.guild.id)
     return commands.check(predicate)
+
+def in_pm():
+    async def predicate(ctx):
+        return isinstance(ctx.channel, discord.DMChannel)
+    return commands.check(predicate)
